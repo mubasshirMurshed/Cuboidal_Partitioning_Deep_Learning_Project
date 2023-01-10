b53caf916a4a9833a5e2cc3824fdf07c0f6d5d7e
Mubasshir_Murshed <mmur0031@student.monash.edu>
2023-01-10 21:11:26 +1100

    Edited init to allow easy importing of all dataset

--- dataModules/__init__.py
+++ dataModules/__init__.py
@@ -1,3 +1,4 @@
 from dataModules.mnistSparseDataModule import *
 from dataModules.mnistOriginalDataModule import *
 from dataModules.mnistBWDataModule import *
+from dataModules.mnistGraphDataModule import *
