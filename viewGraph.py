8b997d40e3c8cb65ee3964b7e8b9fe2e7114f804
Mubasshir_Murshed <mmur0031@student.monash.edu>
2023-01-10 21:06:42 +1100

    Fixed graph data access and edge display issue

--- viewGraph.py
+++ viewGraph.py
@@ -2,31 +2,28 @@
 import matplotlib.pyplot as plt
 from datasets import MNISTGraphDataset
 from torchvision.datasets import MNIST
-from torch_geometric.utils import to_networkx
-import networkx as nx
 
 # %% Dataset
 ds1 = MNIST(r"data\mnistPytorch", train=False)
-ds2 = MNISTGraphDataset(r"data\mnistNew\mnistTest128.csv", 10)
-
-# %% Choose image start
-idx = 1
+ds2 = MNISTGraphDataset(root="data/mnistNew", filename="mnistTest128", length=10)
 
 # %% Plot graph using feature values of x-y pos and colour
+idx = 1
+
 fig, axes = plt.subplots(nrows=1, ncols=2)
 ax1 = axes[0]
 ax2 = axes[1]
 i = 0
 # for i, (ax1, ax2) in enumerate(axes):
 img = ds1[idx + i][0]
-data = ds2.dataset[idx + i].x
-edges_src = ds2.dataset[idx + i].edge_index[0]
-edges_dst = ds2.dataset[idx + i].edge_index[1]
+data = ds2.get(idx + i).x
+values, _ = ds2.get(idx + i).edge_index.t().sort()
+edges = values.unique(dim=0)
 
 ax1.imshow(img, cmap="gray")
 
 ax2.invert_yaxis()
-# ax2.set_facecolor("black")
+ax2.set_facecolor("black")
 ax2.set_xlim([0, 27])
 ax2.xaxis.set_ticks(range(0, 28, 3))
 ax2.set_ylim([27, 0])
@@ -34,18 +31,18 @@ ax2.yaxis.set_ticks(range(27, -1, -3))
 
 # Add nodes
 for i in range(len(data)):
-    ax2.add_patch(plt.Circle((data[i][0]*28, data[i][1]*28), 0.35, facecolor=str(data[i][2].item()), edgecolor="white", linewidth=0.2))
+    ax2.add_patch(plt.Circle((data[i][0]*28, data[i][1]*28), 0.25, facecolor=str(data[i][2].item()), edgecolor="white", linewidth=0.2))
 
 # Add edges
-for i in range(0, len(edges_src), 2):
+for i in range(len(edges)):
     # Get source cuboid information
-    src_cuboid = data[edges_src[i]]
+    src_cuboid = data[edges[i][0]]
     x_src = src_cuboid[0]*28
     y_src = src_cuboid[1]*28
     col_src = src_cuboid[2]
 
     # Get destination cuboid information
-    dst_cuboid = data[edges_src[i+1]]
+    dst_cuboid = data[edges[i][1]]
     x_dst = dst_cuboid[0]*28
     y_dst = dst_cuboid[1]*28
     col_dst = dst_cuboid[2]
@@ -54,7 +51,7 @@ for i in range(0, len(edges_src), 2):
     colour = (col_src + col_dst)/2
 
     # Plot line from (x_src, y_src) to (x_dst, y_dst)
-    ax2.plot([x_src, x_dst], [y_src, y_dst], color=str(colour.item()), linestyle='-', linewidth=1)
+    ax2.plot([x_src, x_dst], [y_src, y_dst], color=str(colour.item()), linestyle='-', linewidth=0.5)
 
 ax2.set_box_aspect(1)
 plt.show()
