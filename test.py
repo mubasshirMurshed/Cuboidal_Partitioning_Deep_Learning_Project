d9c4ba9f87d90f1cb874fb3f94c1a2d11b87ef79
Mubasshir_Murshed <mmur0031@student.monash.edu>
2023-01-14 23:25:04 +1100

    Added 5000/1000 split

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 from dataModules.mnistGraphDataModule import MNISTGraphDataModule
 
 # %% Run data module
-dm = MNISTGraphDataModule(10, [100, 100])
+dm = MNISTGraphDataModule(10, [5000, 1000])
 dm.setup()
 
 # %%
