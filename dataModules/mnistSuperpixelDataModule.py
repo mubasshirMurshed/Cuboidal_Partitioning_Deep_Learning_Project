949e90e5e5b4f788691fc26ea4a611d08ceb8b14
Mubasshir_Murshed <mmur0031@student.monash.edu>
2023-01-14 18:26:36 +1100

    Created superpixel data module

---
+++ dataModules/mnistSuperpixelDataModule.py
@@ -0,0 +1,31 @@
+from dataModules.dataModule import DataModule
+from torch_geometric.loader import DataLoader
+from torch_geometric.datasets import MNISTSuperpixels
+
+
+class MNISTSuperpixelDataModule(DataModule):
+    """
+    A data module for the superpixel MNIST dataset
+    """
+    def __init__(self, train_dir: str, val_dir: str, batch_size: int):
+        """
+        Save attributes.
+
+        Args:
+        - train_dir: str
+            - Directory of training dataset
+        - val_dir: str
+            - Directory of validation dataset
+        - batch_size: int
+            - How many data samples per batch to be loaded
+        """
+        super().__init__(None, None, batch_size, DataLoader)
+    
+
+    def setup(self):
+        """
+        Instantiate datasets for training and validation.
+        """
+        self.train_set = MNISTSuperpixels(root="data/mnistSuperpixel", train=True)
+
+        self.val_set = MNISTSuperpixels(root="data/mnistSuperpixel", train=False)
