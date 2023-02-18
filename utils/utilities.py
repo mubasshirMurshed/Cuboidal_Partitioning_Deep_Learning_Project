6573dc8736ceae8a15d183fcd3c5794aa06fd083
Mubasshir_Murshed <mmur0031@student.monash.edu>
2023-02-19 01:05:37 +1100

    Added ablation code pathway and file path finding

--- utils/utilities.py
+++ utils/utilities.py
@@ -16,7 +16,7 @@ def dirManager(model: nn.Module, data_module: DataModule):
     # Get name of the model and data module class
     modelName = model._get_name()
     dataModuleName = data_module.__class__.__name__
-    ablationCode = data_module.train_set.ablation_code
+    ablationCode = getattr(data_module.train_set, "ablation_code", None)
 
     # Get current time
     d = datetime.now()
@@ -24,12 +24,17 @@ def dirManager(model: nn.Module, data_module: DataModule):
 
     # Create Run ID and directory paths
     runID =  'Run_ID__' + dateString
-    log_dir = f"saved/{dataModuleName}/{ablationCode}/{modelName}/{runID}"
+    if ablationCode is not None:
+        log_dir = f"saved/{dataModuleName}/{ablationCode}/{modelName}/{runID}"
+    else:
+        log_dir = f"saved/{dataModuleName}/{modelName}/{runID}"
     ckpt_dir = log_dir + "/checkpoints"
+    file_dir = log_dir + "/python_files"
 
     # Create directories if they do not exist
     makedirs(ckpt_dir)
-    return log_dir, ckpt_dir
+    makedirs(file_dir)
+    return log_dir, ckpt_dir, file_dir
 
 
 def count_trainable_parameters(model):
@@ -38,3 +43,7 @@ def count_trainable_parameters(model):
 
 def count_untrainable_parameters(model):
     return sum(p.numel() for p in model.parameters() if not p.requires_grad)
+
+
+def getPythonFilePath(obj):
+    return obj.__module__.replace(".", "/") + ".py"
