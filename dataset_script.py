bfc3d919fbb8ca253269e85940fcb8d517f41218
mubasshirMurshed <mubmurshed@gmail.com>
2024-02-18 13:21:35 +1100

    moved stuff around

--- dataset_script.py
+++
@@ -1,8 +0,0 @@
-from rawMNISTDatasetCreator import rawMNISTDataset
-
-def main():
-    creator = rawMNISTDataset("data/", 16, "CP")
-    creator.create_csv_files(verbose=True)
-
-if __name__ == '__main__':
-    main()
